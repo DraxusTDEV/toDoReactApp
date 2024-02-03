36776018e5fded10c6d34c1288621cdc1ac80e5b
DraxusTDEV <draxustienda@gmail.com>
2024-02-03 18:56:57 -0500

    Remove, update y filter actualizados

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,70 @@ const initialStateTodos = [
 const App = () => {
     const [todos, setTodos] = useState(initialStateTodos);
 
+    const createTodo = (title) => {
+        const newTodo = {
+            id: Date.now(),
+            title,
+            completed: false,
+        };
+
+        setTodos([...todos, newTodo]);
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id != id));
+    };
+
+    const updateTodo = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const computedItemsLeft = todos.filter((todo) => !todo.completed).length;
+
+    const clearCompleted = () => {
+        setTodos(todos.filter((todo) => !todo.completed));
+    };
+
+    const [filter, setFilter] = useState("all");
+
+    const changeFilter = (filter) => setFilter(filter);
+
+    const filterTodos = () => {
+        switch (filter) {
+            case "all":
+                return todos;
+            case "active":
+                return todos.filter((todo) => !todo.completed);
+            case "completed":
+                return todos.filter((todo) => todo.completed);
+            default:
+                todos;
+        }
+    };
+
     return (
         <div className="background min-h-screen  bg-gray-300 bg-contain bg-no-repeat">
             <Header />
 
             <main className="container mx-auto mt-8 px-4">
-                <TodoCreate />
+                <TodoCreate createTodo={createTodo} />
 
-                <TodoList todos={todos} />
+                <TodoList
+                    todos={filterTodos()}
+                    removeTodo={removeTodo}
+                    updateTodo={updateTodo}
+                />
 
-                <TodoComputed />
+                <TodoComputed
+                    computedItemsLeft={computedItemsLeft}
+                    clearCompleted={clearCompleted}
+                />
 
-                <TodoFilter />
+                <TodoFilter changeFilter={changeFilter} filter={filter} />
             </main>
 
             <footer className="mt-8 text-center">
