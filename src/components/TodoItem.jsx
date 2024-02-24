1e5bf9ecfe284684fcc0d134ab9523a9fbc64236
DraxusTDEV <draxustienda@gmail.com>
2024-02-23 20:51:11 -0500

    Drag and Drop implementado

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,26 +1,33 @@
+import React from "react";
 import IconCheck from "./icons/IconCheck";
 import IconCross from "./icons/IconCross";
 
-const TodoItem = ({ todo, removeTodo, updateTodo }) => {
-    const { id, title, completed } = todo;
-    return (
-        <article className="flex gap-4 border-b-2 border-b-gray-400 px-4 py-4 dark:bg-gray-800">
-            <button
-                className={`h-5 w-5 flex-none rounded-full border-2 ${completed ? "flex  items-center justify-center  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" : "inline-block"}`}
-                onClick={() => updateTodo(id)}
+const TodoItem = React.forwardRef(
+    ({ todo, removeTodo, updateTodo, ...props }, ref) => {
+        const { id, title, completed } = todo;
+        return (
+            <article
+                {...props}
+                ref={ref}
+                className="flex gap-4 border-b-2 border-b-gray-400 px-4 py-4 dark:bg-gray-800"
             >
-                {completed && <IconCheck />}
-            </button>
-            <p
-                className={`"grow text-gray-700 dark:text-gray-400 ${completed && "line-through"}`}
-            >
-                {title}
-            </p>
-            <button className="flex-none" onClick={() => removeTodo(id)}>
-                <IconCross />
-            </button>
-        </article>
-    );
-};
+                <button
+                    className={`h-5 w-5 flex-none rounded-full border-2 ${completed ? "flex  items-center justify-center  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" : "inline-block"}`}
+                    onClick={() => updateTodo(id)}
+                >
+                    {completed && <IconCheck />}
+                </button>
+                <p
+                    className={`"grow text-gray-700 dark:text-gray-400 ${completed && "line-through"}`}
+                >
+                    {title}
+                </p>
+                <button className="flex-none" onClick={() => removeTodo(id)}>
+                    <IconCross />
+                </button>
+            </article>
+        );
+    }
+);
 
 export default TodoItem;
